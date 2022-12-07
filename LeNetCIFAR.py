c9a3c9515cb76b9750a2e681b567746bb5d82e97
Meng Xuran <77144096+juve-xx@users.noreply.github.com>
2022-12-07 12:57:13 +0800

    Update LeNetCIFAR.py

--- LeNetCIFAR.py
+++ LeNetCIFAR.py
@@ -45,7 +45,7 @@ dtype = torch.float32
 transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Resize(28),
-     transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5 ))])#这里要观察数据channel是一还是三
+     transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5 ))])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
